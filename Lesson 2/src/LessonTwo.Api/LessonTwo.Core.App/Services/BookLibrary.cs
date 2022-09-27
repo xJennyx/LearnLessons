74b8816d70cd23241690f2a6c45f320d2166c2d9
minaev.maxim <mminmax@gmail.com>
2022-09-27 12:38:02 +0300

    [Lesson-2] Добавил ссылку на проект с библиотекой.

---
+++ Lesson 2/src/LessonTwo.Api/LessonTwo.Core.App/Services/BookLibrary.cs
@@ -0,0 +1,13 @@
+﻿using LessonTwo.Domain.Abstractions;
+using LessonTwo.Domain.Models;
+
+namespace LessonTwo.Core.App.Services;
+
+public static class BookLibrary
+{
+    /// <summary>
+    /// Библиотека книг.
+    /// </summary>
+    public static List<Book> BooksLibrary { get; set; } = new List<Book>();
+}
+
