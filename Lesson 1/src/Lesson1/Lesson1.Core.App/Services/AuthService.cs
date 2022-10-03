5d3d9a78fb561caa4a6201913b2780664a9504eb
Viktor <xjennyx068@gmail.com>
2022-10-03 10:14:53 +0300

    проверка потери проекта и папки

---
+++ Lesson 1/src/Lesson1/Lesson1.Core.App/Services/AuthService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1.Core.App.Services
+{
+    public class AuthService
+    {
+        public AuthService()
+        {
+            Employees = new List<string>
+            {
+                "Петров",
+                "Иванов",
+                "Сидоров"
+            };
+        }
+
+        public List<string> Employees { get; private set; }
+
+        public bool Login(string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                return false;
+            }
+
+            var isEmployeeExist = Employees.Contains(lastName);
+
+            if (isEmployeeExist)
+            {
+                UserSession.Sessions.Add(lastName);
+            }
+
+            return isEmployeeExist;
+        }
+    }
+    public static class UserSession
+    {
+        public static HashSet<string> Sessions { get; set; } = new HashSet<string>();
+    }
+}
+
